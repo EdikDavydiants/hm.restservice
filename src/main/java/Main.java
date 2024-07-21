6244deff54eb6c9edeaecb581d6c4a73e3cd04c5
Edik <davydiantsedik@mail.ru>
2024-07-21 22:57:00 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+import java.util.HashMap;
+
+public class Main {
+
+    public static String URL = "jdbc:mysql://localhost:3306/sakila";
+    public static String USERNAME = "root";
+    public static String PASSWORD = "dfhtut5674ebdfh567j";
+
+
+    public static void main(String[] args) throws SQLException {
+
+        //System.out.println("hell");
+
+//        Connection connection = null;
+//
+//        try {
+//            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+//        } catch (SQLException e) {
+//
+//        }
+//
+//        Statement statement = connection.createStatement();
+//
+//        ResultSet resultSet = statement.executeQuery("select * from players");
+//
+//        while (resultSet.next()) {
+//
+//            int id = resultSet.getInt("id");
+//            System.out.println(id);
+//        }
+    }
+
+
+    
+
+
+}
---
+++ src/main/java/servlet/SomeClass.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+
+@WebServlet(name = "someServlet", value = "/hello")
+public class SomeClass extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.write("Hello!");
+        printWriter.close();
+
+
+    }
+
+
+}
